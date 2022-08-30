f5f0675329fae412cfd18ace960ceaaa6df25107
204G1A0516 <112402816+204G1A0516@users.noreply.github.com>
2022-08-30 11:31:16 +0530

    K. Asha Nidhi Created sample.c

---
+++ OS lab/sample.c
@@ -0,0 +1,5 @@
+/*   Name Of the Candidate: K. Asha Nidhi
+   Roll No: 204G1A0516
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022 */
